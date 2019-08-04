032856fe023c7e779928661d793a767eac6dc205
Robert C Jensen <robert.cole.jensen@gmail.com>
2019-08-04 01:34:50 -0400

    setup basic unit tests

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-all:
+debug:
 	clang -g -o build/dahl -Weverything -Wno-gnu-case-range -Wno-padded src/*.c
+lib:
+	clang -o build/libdahl.dylib -shared src/*.c
+test:
+	clang -Lbuild -lcheck -ldahl -o build/test-dahl  tests/*.c && ./build/test-dahl
---
+++ tests/check_lexer.c
@@ -0,0 +1,42 @@
+#include <check.h>
+#include "../src/lexer.h"
+#include "../src/utils.h"
+
+START_TEST(test_lex_simple_expression)
+{
+    FILE *fd = fopen("tests/fixtures/simple-expression.scm", "r");
+    struct TokenList *tokens = lex(fd);
+    ck_assert_int_eq(STAILQ_FIRST(tokens)->kind, TOKEN_PAREN_L);
+    fclose(fd);
+}
+END_TEST
+
+Suite *lexer_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Lexer");
+
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_lex_simple_expression);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+int main(void)
+{
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = lexer_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
